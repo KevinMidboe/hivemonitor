2d0605e6269bd873977383af49118e952d5d6606
Kevin Midboe <kevin.midboe@gmail.com>
2025-02-09 11:50:23 +0100

    easier to add topics, build options smarter from ENV vars

--- src/lib/mqttClient.ts
+++ src/lib/mqttClient.ts
@@ -4,20 +4,37 @@ import { addHiveMessage, addGatewayMessageQueue } from './store';
 import type { IClientOptions, MqttClient } from 'mqtt';
 import type { IGatewayTelemetry, IHiveTelemetry } from './interfaces/ITelemetry';
 
-const options: IClientOptions = {
-  port: 80,
-  protocolVersion: 5,
-  keepalive: 60,
-  clean: true,
-  reconnectPeriod: 1000,
-  connectTimeout: 30 * 1000,
-  properties: {
-    requestResponseInformation: true,
-    requestProblemInformation: true
+function buildOptions(host: string) {
+  const hostUrlPort = new URL(host).port;
+
+  const options: IClientOptions = {
+    port: hostUrlPort ? Number(hostUrlPort) : 80,
+    protocolVersion: 5,
+    keepalive: 60,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    properties: {
+      requestResponseInformation: true,
+      requestProblemInformation: true
+    }
+  };
+
+  return options;
+}
+
+function messageForwarder(topic: string) {
+  if (topic.includes('/hive/')) {
+    return (d: object) => addHiveMessage(d as IHiveTelemetry);
+  } else if (topic.includes('/gateway/')) {
+    return (d: object) => addGatewayMessageQueue(d as IGatewayTelemetry);
   }
-};
+
+  return () => console.log('WARN! No handler for topic:', topic);
+}
 
 function setupMQTTClient(host: string) {
+  const options = buildOptions(host);
   const mqttClient: MqttClient = mqtt.connect(host, options);
 
   mqttClient.on('connect', () => {
@@ -27,23 +44,17 @@ function setupMQTTClient(host: string) {
   });
 
   mqttClient.on('error', (err) => {
+    console.log('mqtt client error:');
     console.log(err);
   });
 
   // For every message received, check if topic matches any of the message queues
   mqttClient.on('message', (topic: string, message: string) => {
-    if (topic.includes('/hive/')) {
-      const json = parseJSONMessage(message);
-      if (!json) return;
-      addHiveMessage(json as IHiveTelemetry);
-    } else if (topic.includes('/gateway/')) {
-      const json = parseJSONMessage(message);
-      if (!json) return;
-      addGatewayMessageQueue(json as IGatewayTelemetry);
-    } else {
-      console.warn('got message for unexpected topic:', topic);
-      console.warn('unexpected message:', message);
-    }
+    const func = messageForwarder(topic);
+    const json = parseJSONMessage(message);
+    if (!json) return;
+
+    func(json);
   });
 }
 
